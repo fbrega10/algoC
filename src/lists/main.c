654f123fb24875744cbe05be8dfeb930bd6501cc
fbrega10 <102411282+fbrega10@users.noreply.github.com>
2024-10-27 16:39:09 +0100

    main change

--- src/lists/main.c
+++ src/lists/main.c
@@ -3,10 +3,11 @@
 
 int main(int argc, char *argv[]) {
 
-  Lista l = makeLista();
-  addNode(5, &l);
-  addNode(7, &l);
-  printList(l);
-  freeList(l);
+  Pila p = makePila();
+  push(6, &p);
+  push(7, &p);
+  push(8, &p);
+  push(9, &p);
+  printList(p);
   return EXIT_SUCCESS;
 }
