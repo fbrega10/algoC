55ec193e38b16f1c598fbbd66a6fabd72a8bfb82
fbrega10 <102411282+fbrega10@users.noreply.github.com>
2024-11-09 11:45:48 +0100

    added license

--- src/sorting/sort.h
+++ src/sorting/sort.h
@@ -5,3 +5,4 @@ typedef int element;
 void selection_sort(element *array, size_t size);
 void swap(void *pt1, void *pt2);
 void print_array(element *array, size_t size);
+void bubble_sort(element *array, size_t size);
