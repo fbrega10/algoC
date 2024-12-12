93e08053537db9e6d5f4da7a9cd924a924bc3b89
fbrega10 <102411282+fbrega10@users.noreply.github.com>
2024-12-12 19:54:57 +0100

    added merge sort

--- src/UPOalglib_core_toolkit/src/sort.c
+++ src/UPOalglib_core_toolkit/src/sort.c
@@ -30,7 +30,7 @@
 void upo_insertion_sort(void *base, size_t n, size_t size, upo_sort_comparator_t cmp)
 {
     unsigned char * ptr = (unsigned char *) base;
-    for (size_t i = 1; i < n; i++)
+    for (size_t i = 1; i < n; ++i)
     {
         size_t j = i;
         while (j > 0 && cmp(ptr + j * size, ptr + (j-1) * size) < 0)
@@ -43,7 +43,62 @@ void upo_insertion_sort(void *base, size_t n, size_t size, upo_sort_comparator_t
 
 void upo_merge_sort(void *base, size_t n, size_t size, upo_sort_comparator_t cmp)
 {
+    if(n <= 1)
+        return;  
+
+    size_t left_size = n/2;   
+
+    size_t righ_size = n-left_size;
+
+    upo_merge_sort((char*)base, left_size, size, cmp);
+    upo_merge_sort((char*)base + left_size * size, righ_size, size, cmp);
+    upo_merge((char*)base, left_size, righ_size, size, cmp);
 }
+
+void upo_merge(void *base, size_t left_size, size_t righ_size, size_t size, upo_sort_comparator_t cmp){
+
+    unsigned char * aux_array = malloc(size*(left_size + righ_size));
+
+    unsigned char * aux_ptr = aux_array;
+
+    unsigned char * basePtr = (unsigned char*) base;
+
+    unsigned char * i1 = basePtr;
+    unsigned char * i2 = basePtr + size * left_size;
+
+
+    while((i1 != basePtr + size*left_size) && (i2 != basePtr + size*(left_size+righ_size)))
+    {
+        if (cmp(i1, i2) < 0) {    
+            memcpy(aux_ptr, i1, size);
+            aux_ptr += size;
+            i1 += size;
+        } else {
+            memcpy(aux_ptr, i2, size);
+            aux_ptr += size;
+            i2 += size;
+        }
+    }
+
+    while(i1 != basePtr + size*left_size)
+    {
+        memcpy(aux_ptr, i1, size);
+        aux_ptr += size;
+        i1 += size;
+    }
+    while(i2 != basePtr + size*(left_size+righ_size))
+    {
+        memcpy(aux_ptr, i2, size);
+        aux_ptr += size;
+        i2 += size;
+    }
+    memcpy(basePtr, aux_array, size*(left_size+righ_size));
+    free(aux_array);
+}
+
+
+
+
 
 void upo_quick_sort(void *base, size_t n, size_t size, upo_sort_comparator_t cmp)
 {
